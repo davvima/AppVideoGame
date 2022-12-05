6b0bafd9062103b4fae720e8fdf6d47c67ad158f
David Freites <dfreites7@gmail.com>
2022-12-05 14:57:44 -0300

    axios configuration

--- client/src/config/axiosClient.js
+++ client/src/config/axiosClient.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
+import dotenv from "dotenv";
+dotenv.config();
+
+import dotenv from "dotenv";
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3001",
+
     headers: {
         'Content-Type': 'application/json'
     },
