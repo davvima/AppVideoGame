92baec5af8692a507a172d1715f921424d902d06
David Freites <dfreites7@gmail.com>
2022-12-05 15:21:17 -0300

    Test axios

--- client/src/config/axiosClient.js
+++ client/src/config/axiosClient.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
-
-import dotenv from "dotenv";
-dotenv.config();
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const axiosClient = axios.create({
-
+    baseURL: process.env.REACT_APP_API || "http://localhost:3001",
     headers: {
         'Content-Type': 'application/json'
     },
+
     
 });
 
